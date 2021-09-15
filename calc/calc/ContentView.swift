421f34ab28fd0023dc12cbe62ae9be54526d25f7
f6_kovalev <qwerty.018@mail.ru>
2021-09-15 16:38:34 +0300

    itog

---
+++ calc/calc/ContentView.swift
@@ -0,0 +1,215 @@
+//
+//  ContentView.swift
+//  calc
+//
+//  Created by user on 15.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color("BackgroundColor")
+            VStack() {
+                Text("0")
+                    .foregroundColor(.white)
+                    .font(.system(size: 90))
+                    .fontWeight(.thin)
+                    .padding(.horizontal,20)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .trailing)
+                HStack(spacing:1){
+                    Button(action: {
+                    }, label: {
+                        Text("AC")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("FormatColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Image(systemName: "plus.slash.minus")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("FormatColor"))
+                            
+                    })
+                    Button(action: {
+                    }, label: {
+                        Text("%")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("FormatColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Image(systemName: "divide")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("OperColor"))
+                    })
+                }
+                HStack(spacing:1){
+                    Button(action: {
+                    }, label: {
+                        Text("7")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Text("8")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                            
+                    })
+                    Button(action: {
+                    }, label: {
+                        Text("9")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Image(systemName: "multiply")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("OperColor"))
+                    })
+                    .padding(.vertical,1)
+                }
+                HStack(spacing:1){
+                    Button(action: {
+                    }, label: {
+                        Text("4")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Text("5")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                            
+                    })
+                    Button(action: {
+                    }, label: {
+                        Text("6")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Image(systemName: "minus")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("OperColor"))
+                    })
+                }
+                HStack(spacing:1){
+                    Button(action: {
+                    }, label: {
+                        Text("1")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Text("2")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                            
+                    })
+                    Button(action: {
+                    }, label: {
+                        Text("3")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("OperColor"))
+                    })
+                    .padding(.vertical,1)
+                }
+                HStack(spacing:1){
+                    Button(action: {
+                    }, label: {
+                        Text("0")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/2, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                    })
+                    Button(action: {
+                    }, label: {
+                        Text(",")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .fontWeight(.medium)
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("ButtonsColor"))
+                            
+                    })
+                    Button(action: {
+                    }, label: {
+                        Image(systemName: "equal")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/4-1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .background(Color("OperColor"))
+                    })
+                }
+            }.frame(width: UIScreen.main.bounds.width, alignment: .trailing)
+        }.ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+
+        }
+    }
+}
