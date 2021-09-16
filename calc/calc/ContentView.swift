a56177d91821d4b98b42149cea90de999571e6c5
f6_kovalev <qwerty.018@mail.ru>
2021-09-16 14:44:11 +0300

    CalcFull

--- calc/calc/ContentView.swift
+++ calc/calc/ContentView.swift
@@ -8,19 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    // state - если что то меняется в переменной, то нужно менять интерфейс
+    @State var number = "0"
+    @State var alert = false
+    @State var znak = true
+    @State var operation = ""
+    @State var point = false
+    @State var decision = false
+    @State var result: Float = 0
     var body: some View {
         ZStack{
             Color("BackgroundColor")
             VStack(spacing:1) {
                 Spacer()
-                Text("0")
+                Text(number)
                     .foregroundColor(.white)
-                    .font(.system(size: 90))
+                    .font(.system(size: 80))
                     .fontWeight(.thin)
+                    .lineLimit(1)
                     .padding(.horizontal,20)
                     .frame(width: UIScreen.main.bounds.width, alignment: .trailing)
                 HStack(spacing:1){
                     Button(action: {
+                        number = "0"
+                        point = false
+                        decision = false
                     }, label: {
                         Text("AC")
                             .foregroundColor(.white)
@@ -30,6 +42,21 @@ struct ContentView: View {
                             .background(Color("FormatColor"))
                     })
                     Button(action: {
+                        
+                        /*if znak == true {
+                            number.insert("-", at: number.startIndex)
+                            znak = false
+                        }
+                        else {
+                            number.removeFirst()
+                            znak = true
+                        }*/
+                        if number.contains("-") {
+                            number.removeFirst()
+                        }
+                        else {
+                            number = "-" + number
+                        }
                     }, label: {
                         Image(systemName: "plus.slash.minus")
                             .foregroundColor(.white)
@@ -39,6 +66,7 @@ struct ContentView: View {
                             
                     })
                     Button(action: {
+                        number = String(Float(number)!/100)
                     }, label: {
                         Text("%")
                             .foregroundColor(.white)
@@ -48,6 +76,10 @@ struct ContentView: View {
                             .background(Color("FormatColor"))
                     })
                     Button(action: {
+                        operation = "/"
+                        result = result / Float(number)!
+                        number = "0"
+                        point = false
                     }, label: {
                         Image(systemName: "divide")
                             .foregroundColor(.white)
@@ -58,6 +90,13 @@ struct ContentView: View {
                 }
                 HStack(spacing:1){
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "7"
+                            decision = true
+                        }
+                        else {
+                            number.append("7")
+                        }
                     }, label: {
                         Text("7")
                             .foregroundColor(.white)
@@ -67,6 +106,13 @@ struct ContentView: View {
                             .background(Color("ButtonsColor"))
                     })
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "8"
+                            decision = true
+                        }
+                        else {
+                            number.append("8")
+                        }
                     }, label: {
                         Text("8")
                             .foregroundColor(.white)
@@ -77,6 +123,13 @@ struct ContentView: View {
                             
                     })
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "9"
+                            decision = true
+                        }
+                        else {
+                            number.append("9")
+                        }
                     }, label: {
                         Text("9")
                             .foregroundColor(.white)
@@ -86,6 +139,10 @@ struct ContentView: View {
                             .background(Color("ButtonsColor"))
                     })
                     Button(action: {
+                        operation = "*"
+                        result = result * Float(number)!
+                        number = "0"
+                        point = false
                     }, label: {
                         Image(systemName: "multiply")
                             .foregroundColor(.white)
@@ -96,6 +153,13 @@ struct ContentView: View {
                 }
                 HStack(spacing:1){
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "4"
+                            decision = true
+                        }
+                        else {
+                            number.append("4")
+                        }
                     }, label: {
                         Text("4")
                             .foregroundColor(.white)
@@ -105,6 +169,13 @@ struct ContentView: View {
                             .background(Color("ButtonsColor"))
                     })
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "5"
+                            decision = true
+                        }
+                        else {
+                            number.append("5")
+                        }
                     }, label: {
                         Text("5")
                             .foregroundColor(.white)
@@ -115,6 +186,13 @@ struct ContentView: View {
                             
                     })
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "6"
+                            decision = true
+                        }
+                        else {
+                            number.append("6")
+                        }
                     }, label: {
                         Text("6")
                             .foregroundColor(.white)
@@ -124,6 +202,10 @@ struct ContentView: View {
                             .background(Color("ButtonsColor"))
                     })
                     Button(action: {
+                        operation = "-"
+                        result = result - Float(number)!
+                        number = "0"
+                        point = false
                     }, label: {
                         Image(systemName: "minus")
                             .foregroundColor(.white)
@@ -134,6 +216,13 @@ struct ContentView: View {
                 }
                 HStack(spacing:1){
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "1"
+                            decision = true
+                        }
+                        else {
+                            number.append("1")
+                        }
                     }, label: {
                         Text("1")
                             .foregroundColor(.white)
@@ -143,6 +232,13 @@ struct ContentView: View {
                             .background(Color("ButtonsColor"))
                     })
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "2"
+                            decision = true
+                        }
+                        else {
+                            number.append("2")
+                        }
                     }, label: {
                         Text("2")
                             .foregroundColor(.white)
@@ -153,6 +249,13 @@ struct ContentView: View {
                             
                     })
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "3"
+                            decision = true
+                        }
+                        else {
+                            number.append("3")
+                        }
                     }, label: {
                         Text("3")
                             .foregroundColor(.white)
@@ -162,6 +265,10 @@ struct ContentView: View {
                             .background(Color("ButtonsColor"))
                     })
                     Button(action: {
+                        operation = "+"
+                        result = result + Float(number)!
+                        number = "0"
+                        point = false
                     }, label: {
                         Image(systemName: "plus")
                             .foregroundColor(.white)
@@ -173,6 +280,13 @@ struct ContentView: View {
                 }
                 HStack(spacing:1){
                     Button(action: {
+                        if number == "0" || decision == false{
+                            number = "0"
+                            decision = true
+                        }
+                        else {
+                            number.append("0")
+                        }
                     }, label: {
                         Text("0")
                             .foregroundColor(.white)
@@ -182,6 +296,10 @@ struct ContentView: View {
                             .background(Color("ButtonsColor"))
                     })
                     Button(action: {
+                        if point == false {
+                            number.append(".")
+                            point = true
+                        }
                     }, label: {
                         Text(",")
                             .foregroundColor(.white)
@@ -192,6 +310,31 @@ struct ContentView: View {
                             
                     })
                     Button(action: {
+                        switch operation {
+                        case "/":
+                            result = result / Float(number)!
+                        case "*":
+                            result = result * Float(number)!
+                        case "-":
+                            result = result - Float(number)!
+                        case "+":
+                            result = result + Float(number)!
+                        default:
+                            break
+                        }
+                        /*
+                        if result - result.rounded() == 0 {
+                            number = String(Int(result))
+                        }
+                        else {
+                            number = String(result)
+                        }
+                        */
+                        number = result - result.rounded() == 0 ? String(Int(result)) : String(result)
+                        
+                        operation = ""
+                        result = 0
+                        decision = false
                     }, label: {
                         Image(systemName: "equal")
                             .foregroundColor(.white)
@@ -201,7 +344,15 @@ struct ContentView: View {
                     })
                 }
             }.frame(width: UIScreen.main.bounds.width, alignment: .trailing)
-            
+            .alert(isPresented: $alert, content: {
+                Alert(title: Text("Error"), message: Text("def"), dismissButton: .cancel())
+            })
+            .onChange(of: number, perform: { value in
+                if number.count > 7 {
+                    alert.toggle()
+                    number.removeLast()
+                }
+            })
         }.ignoresSafeArea()
     }
 }
